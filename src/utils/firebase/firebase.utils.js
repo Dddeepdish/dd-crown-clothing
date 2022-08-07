ab02cde5cd44fc1f278708c84352c895b675ba09
Masood Cates <106396451+Dddeepdish@users.noreply.github.com>
2022-08-07 09:16:59 -0400

    Added firestore (#3)
    * added routes
    
    * Added Google Authentication with Firebase
    
    * Added sign  in component as well as firebase utils and libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,12 @@ import {
     signInWithPopup,
     GoogleAuthProvider
 } from 'firebase/auth';
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyA2exRSIUPdvfQcEgP_MTDDD5jQqPlRxwY",
@@ -25,3 +31,31 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromauth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+        console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+}
